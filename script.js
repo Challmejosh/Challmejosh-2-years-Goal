5775f6c939211494ec59bd37b79ac215af2204c3
Challmejosh <134891799+Challmejosh@users.noreply.github.com>
2024-07-04 15:34:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const d = new Date();
 const time = document.querySelector("#currentTimeUTC");
- time.textContext = d.toUTCString();
+time.textContext = d.toUTCString();
 
 const p = new Date();
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
